0049b76e9ef532d07aa41cbef99f7c24adb57838
CarlesSirvelia <carles@sirvelia.com>
2022-10-12 17:56:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import IA.Energia.Centrales;
+import IA.Energia.VEnergia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // VEnergia;
+        // Centrales;
+        // Clientes;
+    }
+}
