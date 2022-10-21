584f41f1553cca2036e891a91c189146560f2740
nmayol <02mayolneus@gmail.com>
2022-10-21 12:45:32 +0200

    simmulated annealing

---
+++ src/aima/EnergiaSuccessorFunctionSimmulatedAnnealing.java
@@ -0,0 +1,11 @@
+package aima;
+import java.util.List;
+import aima.search.framework.SuccessorFunction;
+
+public class EnergiaSuccessorFunctionSimmulatedAnnealing implements SuccessorFunction {
+
+    @Override
+    public List getSuccessors(Object o) {
+        return null;
+    }
+}
