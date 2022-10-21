1f9a7f19b16bfd8ab47ad3a7e06a9b2f31a32419
CarlesSirvelia <carles@sirvelia.com>
2022-10-21 02:17:23 +0200

    Estat readaptat, funcions auxiliars, estat inicial random

--- src/aima/EnergiaEstado.java
+++ src/aima/EnergiaEstado.java
@@ -3,18 +3,16 @@ package aima;
 import IA.Energia.Centrales;
 import IA.Energia.Clientes;
 import IA.Energia.VEnergia;
-import com.sun.tools.javac.Main;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class EnergiaEstado {
-    private ArrayList<ArrayList<Integer>> centrales_con_clientes;
+    private int[] clientes_assignados;
     private static Centrales centrales;
     private static Clientes clientes;
 
-    public EnergiaEstado(ArrayList<ArrayList<Integer>> centrales_con_clientes) {
-        this.centrales_con_clientes = centrales_con_clientes;
+    public EnergiaEstado(int[] clientes_assignados) {
+        this.clientes_assignados = clientes_assignados;
     }
 
     /*
@@ -24,22 +22,22 @@ public class EnergiaEstado {
     public static EnergiaEstado estadoInicial(Centrales ce, Clientes cl, int semilla) {
         centrales = ce;
         clientes = cl;
-        EnergiaEstado estado = new EnergiaEstado(new ArrayList<ArrayList<Integer>>());
-        estado.centrales_con_clientes = new ArrayList<ArrayList<Integer>>();
-
-        // Inicializar centrales sin ningun cliente assignado
-        for (int i = 0; i < centrales.size(); i++) {
-            estado.centrales_con_clientes.add(new ArrayList<Integer>());
-        }
+        EnergiaEstado estado = new EnergiaEstado(new int[centrales.size()]);
 
         Random random = new Random((long) semilla);
-        for (int i_cliente = 0; i_cliente < clientes.size(); i_cliente++) { // Todos los clientes
-            if (clientes.get(i_cliente).getContrato() == 0) { // Cliente de prioridad garantizada
-                boolean cliente_assignado = false;
-                while ( !cliente_assignado ) {
+
+        for (int i_cliente = 0; i_cliente < estado.clientes_assignados.length; i_cliente++) { // Todos los clientes
+            if ( clientes.get(i_cliente).getContrato() == 0 ) { // clientes garantizados
+                boolean cli_asignado = false;
+                while ( !cli_asignado ) { // Cliente de prioridad garantizada
                     int central_random = random.nextInt(centrales.size());
-                    cliente_assignado = estado.anadir_cliente();
+                    if (estado.sePuedeMoverCliente(i_cliente, central_random)) {
+                        estado.moverCliente(i_cliente, central_random);
+                        cli_asignado = true;
+                    }
                 }
+            } else { // Clientes no garantizados
+                estado.moverCliente(i_cliente, -1); // Colocamos fuera de ninguna central
             }
         }
 
@@ -50,76 +48,48 @@ public class EnergiaEstado {
     /*
      * OPERADORES
      */
-    public boolean anadir_cliente () {
-
-        return true;
+    // Pre: Que el cliente tenga lugar en la central destino o lo quiten de la central y sea NO garantizado
+    public void moverCliente(int i_cliente, int centralDestino) {
+        clientes_assignados[i_cliente] = centralDestino;
     }
 
-    public boolean quitar_cliente (int central, int cliente) {
-
-        return true;
-    }
-
-    public boolean intercanviar_cliente () {
-
-        return true;
-    }
 
     /*
-    * Beneficio del estado
-    */
-    public double beneficioTotal() {
-        VEnergia vEnergia;
-        int gasto = 0;
-        int ingreso = 0;
-        for (int i_central = 0; i_central < centrales_con_clientes.size(); i_central++) {
-            int tipo_central = centrales.get(i_central).getTipo();
-
-            if (centrales_con_clientes.get(i_central).size() != 0) { // Tiene alguna central asignada
-                // Esta en funcionamiento
-                try {
-                    double produccion_central = centrales.get(i_central).getProduccion();
-                    double costeMW = VEnergia.getCosteProduccionMW(tipo_central);
-                    double costeEncender = VEnergia.getCosteMarcha(tipo_central);
-                    gasto += produccion_central * costeMW + costeEncender;
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                // Esta apagada
-                try {
-                    double costeApagada = VEnergia.getCosteParada(tipo_central);
-                    gasto += costeApagada;
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            /*
-             *  FALTAN LAS PENALIZACIONES DE LOS NO GARANTIZADOS !!!!!!!
-             */
-
-            for (int i_cliente = 0; i_cliente < centrales_con_clientes.get(i_central).size(); i_cliente++) {
-                int tipo_cliente = clientes.get(i_cliente).getTipo();
-                int contrato_cliente = clientes.get(i_cliente).getContrato();
-                double precio_mw = 0;
-                try {
-                    precio_mw = clientes.get(i_cliente).getContrato() == 1 ? VEnergia.getTarifaClienteNoGarantizada(tipo_cliente) : VEnergia.getTarifaClienteGarantizada(tipo_cliente);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-                double consumo = clientes.get(i_cliente).getConsumo();
-
-                ingreso += consumo * precio_mw;
-            }
-        }
-        return ingreso - gasto;
-    }
-
-    /*
-     * Distancia entre 1 central y 1 cliente
+     * COMPROVADORES
      */
-    private double distanciaCentralCliente(int i_central, int i_cliente) {
+    // Comprovamos que si la central destino es fuera, el cliente sea NO prioritario/garantizado,
+    // de lo contrario tenga sitio para este cliente
+    public boolean sePuedeMoverCliente(int i_cliente, int i_centralDestino) {
+        if ( (i_centralDestino == -1 && clientes.get(i_cliente).getContrato() == 1) || consumoMasPerdidas(i_centralDestino, i_cliente) < energiaSobranteCentral(i_centralDestino) )
+            return true;
+        return false;
+    }
+
+
+    // Beneficio obtenido del estado
+    public double beneficioTotal() {
+        double sumaBeneficios = 0.0;
+        for ( int i_central = 0; i_central < centrales.size(); i_central++) {
+            sumaBeneficios += beneficioCentral(i_central);
+        }
+        return sumaBeneficios;
+    }
+
+    // Imprimir el estado
+    public void print() {
+        System.out.println("------ ESTADO -------");
+        for (int i_cliente = 0; i_cliente < clientes_assignados.length; i_cliente++) { // Centrales
+            System.out.println("Cliente " + i_cliente + " -> " + clientes_assignados[i_cliente]);
+        }
+        System.out.println("---------------------");
+    }
+
+    /*
+     * FUNCIONES AUXILIARES
+     */
+
+    // Distancia entre 1 central y 1 cliente
+    private double distanciaCentralCliente (int i_central, int i_cliente) {
         int ce_x = centrales.get(i_central).getCoordX();
         int ce_y = centrales.get(i_central).getCoordY();
         int cl_x = clientes.get(i_cliente).getCoordX();
@@ -127,14 +97,36 @@ public class EnergiaEstado {
         return Math.sqrt( Math.pow(ce_x-cl_x, 2) + Math.pow(ce_y-cl_y, 2)); // sqrt( dX^2 + dY^2)
     }
 
-    public void print() {
-        System.out.println("------ ESTADO -------");
-        for (int i_central = 0; i_central < centrales_con_clientes.size(); i_central++) { // Centrales
-            System.out.println("Central " + i_central + ":");
-            for (int i_cliente = 0; i_cliente < centrales_con_clientes.get(i_central).size(); i_cliente++) { // Clientes de esa central
-                System.out.println("\tCliente " + i_cliente);
+    // Suma de todas las distancias de los
+    private double sumaTodasLasDistancias () {
+        double sumDist = 0.0;
+        for (int i_cli = 0; i_cli < clientes_assignados.length; i_cli++) {
+            if ( clientes_assignados[i_cli] != -1 )
+                sumDist += distanciaCentralCliente(i_cli, clientes_assignados[i_cli]);
+        }
+        return sumDist;
+    }
+
+    // Consumo de un cliente contando perdidas por la distancia a la central
+    private double consumoMasPerdidas (int i_central, int i_cliente) {
+        double dist = distanciaCentralCliente(i_central, i_cliente);
+        return clientes.get(i_cliente).getConsumo() * (1+VEnergia.getPerdida(dist));
+    }
+
+    // Cantidad de energia que ya esta asignada
+    private double energiaAssignadaCentral (int i_central) {
+        double sumEnergiaUsada = 0.0;
+        for (int i_cli = 0; i_cli < clientes_assignados.length; i_cli++) {
+            if (clientes_assignados[i_cli] == i_central) {
+                sumEnergiaUsada += consumoMasPerdidas(i_central, i_cli);
             }
         }
-        System.out.println("---------------------");
+        return sumEnergiaUsada;
     }
+
+    // Cantidad de energia que puede ser asignada aun
+    private double energiaSobranteCentral (int i_central) {
+        return centrales.get(i_central).getProduccion() - energiaAssignadaCentral(i_central);
+    }
+
 }
